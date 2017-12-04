e09aaaac7e536d9b8ebd42a6f9d5ddaf8fb8d48f
kyleashcraft <ashcraft.k@husky.neu.edu>
2017-12-04 15:14:56 -0800

    added comment and post seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+require 'random_data'
+
+50.times do
+  Post.create!(
+    title: RandomData.random_sentence,
+    body: RandomData.random_paragraph
+  )
+end
+posts = Post.all
+
+puts "#{posts.count}"
+unique_post = Post.find_or_create_by(title: "test post", body: "test post body")
+puts "#{posts.count}"
+
+100.times do
+  Comment.create!(
+    post: posts.sample,
+    body: RandomData.random_paragraph
+  )
+end
+puts "#{Comment.count}"
+Comment.find_or_create_by(post: unique_post, body: "test comment body")
+puts "#{Comment.count}"
+
+puts "Seed finished"
+puts "#{Post.count} posts created"
+puts "#{Comment.count} comments created"
