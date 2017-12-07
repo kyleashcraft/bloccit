2c8b7ebc9a8d5f2877a139f3416cab1907f3b43e
kyleashcraft <ashcraft.k@husky.neu.edu>
2017-12-06 18:35:30 -0800

    added a spam filter to posts_controller.rb

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,9 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.all
+    @posts.each_with_index do |p,i|
+      p.title = "SPAM" if i % 5 == 0
+    end
   end
 
   def show
