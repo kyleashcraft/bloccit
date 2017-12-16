690a01118f8478d34e0792b8ffd17bffa576c6f4
kyleashcraft <ashcraft.k@husky.neu.edu>
2017-12-16 13:26:59 -0800

    associated posts and users

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,21 @@
 
 require 'random_data'
 
+5.times do
+  User.create!(
+    name: RandomData.random_name,
+    email: RandomData.random_email,
+    password: RandomData.random_sentence
+  )
+end
+users = User.all
+
+user = User.first
+user.update_attributes!(
+  email: 'kylehashcraft@gmail.com',
+  password: 'password'
+)
+
 15.times do
   Topic.create!(
     name: RandomData.random_sentence,
@@ -18,6 +33,7 @@ topics = Topic.all
 
 50.times do
   Post.create!(
+    user: users.sample,
     topic: topics.sample,
     title: RandomData.random_sentence,
     body: RandomData.random_paragraph
@@ -51,6 +67,7 @@ end
 puts "#{Question.count}"
 puts "\n \n"
 puts "Seed finished"
+puts "#{User.count} users created"
 puts "#{Topic.count} topics created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
